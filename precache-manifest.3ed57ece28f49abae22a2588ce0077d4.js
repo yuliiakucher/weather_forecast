f609dbe6b102d3903197ae5250af1203f91e40e8
Yuliia Kucher <yuliia.s.kucher@gmail.com>
2020-10-27 23:08:06 +0200

    Updates

---
+++ precache-manifest.3ed57ece28f49abae22a2588ce0077d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0c4794dd3a118d67709c344643158b",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "09bcd51965ed1718bb87",
+    "url": "/weather_forecast/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "61fe43d3d86f6dc0cd37",
+    "url": "/weather_forecast/static/css/main.2865bde2.chunk.css"
+  },
+  {
+    "revision": "09bcd51965ed1718bb87",
+    "url": "/weather_forecast/static/js/2.429d16d4.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/weather_forecast/static/js/2.429d16d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fe43d3d86f6dc0cd37",
+    "url": "/weather_forecast/static/js/main.e5aa3e56.chunk.js"
+  },
+  {
+    "revision": "be80f3829e0a3f6babd2",
+    "url": "/weather_forecast/static/js/runtime-main.33f6a04d.js"
+  }
+]);
